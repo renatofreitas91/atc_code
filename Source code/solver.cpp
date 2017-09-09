aaecb80810276f36725cdb0b9314ae4aed15a10a
renatofreitas91 <renato_freitas91@hotmail.com>
2017-09-09 13:00:45 +0100

    Improved solver.

--- Source code/solver.cpp
+++ Source code/solver.cpp
@@ -16,24 +16,22 @@ double solver(char expression[DIM]) {
 	char equation[DIM] = "", saveEquation[DIM] = "";
 	sprintf(equation, "%s", expression);
 	char const * equationF = equation;
-	double precisionR = 0.01, precisionI = 0, resultFR = -0.1, resultFI = 0, savePrecisionR = 0.01, savePrecisionI = 0, saveResultR = -0.1, saveResultI = 0, time_s = 0, time_ms = 0, save_time = 0;
+	double precisionR = 0.01, precisionI = 0, resultFR = -0.1, resultFI = 0, savePrecisionR = 0.01, savePrecisionI = 0, saveResultR = -0.1, saveResultI = 0;
 	char Xequal[100] = "";
-	int waitTime = 60, timesToEvaluate = 300, timesEvaluated = 0, interactions = 30;
+	int waitTime = 60, timesToEvaluate = 420, timesEvaluated = 0, interactions = 30;
 	boolean initialR = true, initialI = true, imaginary = true, counter = true;
 	if (retrySolver == (boolean)true) {
 		precisionI = 0.01; resultFI = -0.1;
 		savePrecisionI = 0.01; saveResultI = -0.1;
 	}
-	clock_t start, end;
 	resultR = resultFR;
 	resultI = resultFI;
-	start = clock();
 	boolean to_solve = dataVerifier(equation, 0, 0, 1, 1);
 	if (to_solve) {
 		xValuesR = resultFR; xValuesI = resultFI;
 		sprintf(equation, "%s", equationF);
 		calcNow(equation, 0, 0);
-		while (isSolved() == false && time_s < waitTime && timesEvaluated < timesToEvaluate) {
+		while (isSolved() == false && timesEvaluated < timesToEvaluate) {
 			timesEvaluated++;
 			if (resultFR >= (saveResultR*-1) || initialR == (boolean)true) {
 				saveResultR = saveResultR * 10;
@@ -42,25 +40,16 @@ double solver(char expression[DIM]) {
 				resultFR = saveResultR;
 				initialR = false;
 			}
-			if (resultFI >= (saveResultI*-1) || initialI == (boolean)true) {
-				if (((resultI <-1E-7 || resultI>1E-7) && imaginary == (boolean)true)) {
-					resultFI = -0.1;
-					saveResultI = -0.1;
-					precisionI = 0.01;
-					savePrecisionI = 0.01;
-					imaginary = false;
-				}
-				else {
-					if (imaginary == (boolean)false) {
-						saveResultI = saveResultI * 10;
-						savePrecisionI = savePrecisionI * 10;
-						precisionI = savePrecisionI;
-						resultFI = saveResultI;
-						initialI = false;
-					}
+			if (retrySolver == (boolean)true) {
+				if (resultFI >= (saveResultI*-1) || initialI == (boolean)true) {
+					saveResultI = saveResultI * 10;
+					savePrecisionI = savePrecisionI * 10;
+					precisionI = savePrecisionI;
+					resultFI = saveResultI;
+					initialI = false;
 				}
 			}
-			while (resultR != 0 && resultFR < (saveResultR*-1) && time_s < waitTime && timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0) {
+			while (resultR != 0 && resultFR < (saveResultR*-1) && timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0) {
 				timesEvaluated++;
 				xValuesR = resultFR; xValuesI = resultFI;
 				sprintf(equation, "%s", equationF);
@@ -81,13 +70,7 @@ double solver(char expression[DIM]) {
 							sprintf(equation, "%s", equationF);
 							calcNow(equation, 0, 0);
 						}
-						end = clock();
-						time_ms = (end - start) / (CLOCKS_PER_SEC / 1000);
-						time_s = qu(time_ms, 1000);
-						if (save_time != time_s) {
-							save_time = time_s;
-						}
-					} while (resultR != 0 && resultFR < (saveResultR*-1) && time_s < waitTime&& timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0);
+					} while (resultR != 0 && resultFR < (saveResultR*-1) && timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0);
 				}
 				else {
 					do {
@@ -107,116 +90,66 @@ double solver(char expression[DIM]) {
 								calcNow(equation, 0, 0);
 							}
 						}
-						end = clock();
-						time_ms = (end - start) / (CLOCKS_PER_SEC / 1000);
-						time_s = qu(time_ms, 1000);
-						if (save_time != time_s) {
-							save_time = time_s;
-						}
-					} while (resultR != 0 && resultFR < (saveResultR*-1) && time_s < waitTime&& timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0);
+					} while (resultR != 0 && resultFR < (saveResultR*-1) && timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0);
 
 				}
-				if (time_s >= waitTime) {
-					printf("\n\nError: Unable to find value for x.\n\n");
-					solverRunning = false;
-					solving = true;
-					return -7000;
-				}
-				else {
-					if (resultR == 0) {
-						break;
-					}
-				}
-				end = clock();
-				time_ms = (end - start) / (CLOCKS_PER_SEC / 1000);
-				time_s = qu(time_ms, 1000);
-				if (save_time != time_s) {
-					save_time = time_s;
+				if (resultR == 0) {
+					break;
 				}
 			}
 			timesEvaluated++;
-			while (resultI != 0 && resultFI < (saveResultI*-1) && time_s < waitTime && timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0) {
-				timesEvaluated++;
-				xValuesR = resultFR; xValuesI = resultFI;
-				sprintf(equation, "%s", equationF);
-				calcNow(equation, 0, 0);
-				if (resultI < 0) {
-					do {
-						timesEvaluated++;
-						if (resultI < 0) {
-							resultFI = resultFI + precisionI;
-							xValuesR = resultFR; xValuesI = resultFI;
-							sprintf(equation, "%s", equationF);
-							calcNow(equation, 0, 0);
-						}
-						if (resultI > 0) {
-							resultFI = resultFI - precisionI;
-							precisionI = precisionI / 10;
-							xValuesR = resultFR; xValuesI = resultFI;
-							sprintf(equation, "%s", equationF);
-							calcNow(equation, 0, 0);
-						}
-						end = clock();
-						time_ms = (end - start) / (CLOCKS_PER_SEC / 1000);
-						time_s = qu(time_ms, 1000);
-						if (save_time != time_s) {
-							save_time = time_s;
-						}
-					} while (resultI != 0 && resultFI < (saveResultI*-1) && time_s < waitTime&& timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0);
-				}
-				else {
-					do {
-						timesEvaluated++;
-						if (resultI > 0) {
-							resultFI = resultFI + precisionI;
-							xValuesR = resultFR; xValuesI = resultFI;
-							sprintf(equation, "%s", equationF);
-							calcNow(equation, 0, 0);
-						}
-						else {
+			if (retrySolver == (boolean)true) {
+				while (resultI != 0 && resultFI < (saveResultI*-1) && timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0) {
+					timesEvaluated++;
+					xValuesR = resultFR; xValuesI = resultFI;
+					sprintf(equation, "%s", equationF);
+					calcNow(equation, 0, 0);
+					if (resultI < 0) {
+						do {
+							timesEvaluated++;
 							if (resultI < 0) {
+								resultFI = resultFI + precisionI;
+								xValuesR = resultFR; xValuesI = resultFI;
+								sprintf(equation, "%s", equationF);
+								calcNow(equation, 0, 0);
+							}
+							if (resultI > 0) {
 								resultFI = resultFI - precisionI;
 								precisionI = precisionI / 10;
 								xValuesR = resultFR; xValuesI = resultFI;
 								sprintf(equation, "%s", equationF);
 								calcNow(equation, 0, 0);
 							}
-						}
-						end = clock();
-						time_ms = (end - start) / (CLOCKS_PER_SEC / 1000);
-						time_s = qu(time_ms, 1000);
-						if (save_time != time_s) {
-							save_time = time_s;
-						}
-					} while (resultI != 0 && resultFI < (saveResultI*-1) && time_s < waitTime&& timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0);
+						} while (resultI != 0 && resultFI < (saveResultI*-1) && timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0);
+					}
+					else {
+						do {
+							timesEvaluated++;
+							if (resultI > 0) {
+								resultFI = resultFI + precisionI;
+								xValuesR = resultFR; xValuesI = resultFI;
+								sprintf(equation, "%s", equationF);
+								calcNow(equation, 0, 0);
+							}
+							else {
+								if (resultI < 0) {
+									resultFI = resultFI - precisionI;
+									precisionI = precisionI / 10;
+									xValuesR = resultFR; xValuesI = resultFI;
+									sprintf(equation, "%s", equationF);
+									calcNow(equation, 0, 0);
+								}
+							}
+						} while (resultI != 0 && resultFI < (saveResultI*-1) && timesEvaluated < timesToEvaluate && timesEvaluated % interactions != 0);
 
-				}
-				if (time_s >= waitTime) {
-					printf("\n\nError: Unable to find value for x.\n\n");
-					solverRunning = false;
-					solving = true;
-					return -7000;
-				}
-				else {
+					}
 					if (resultI == 0) {
 						break;
 					}
 				}
-				end = clock();
-				time_ms = (end - start) / (CLOCKS_PER_SEC / 1000);
-				time_s = qu(time_ms, 1000);
-				if (save_time != time_s) {
-					save_time = time_s;
-				}
-			}
-			end = clock();
-			time_ms = (end - start) / (CLOCKS_PER_SEC / 1000);
-			time_s = qu(time_ms, 1000);
-			if (save_time != time_s) {
-				save_time = time_s;
 			}
 		}
-		if (retrySolver == (boolean)false && (resultI > -1E-7&&resultI < 1E-7) && (resultR > -1E-7&&resultR < 1E-7) == false) {
+		if (retrySolver == (boolean)false && ((resultR > -1E-2&&resultR < 1E-2) == false || (resultI > -1E-2&&resultI < 1E-2) == false)) {
 			retrySolver = true;
 			solver(expression);
 			resultFR = resultR; resultFI = resultI;
